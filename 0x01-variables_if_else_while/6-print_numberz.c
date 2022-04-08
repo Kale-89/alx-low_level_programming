0f9336fb06c043656674ec87621898873714fd92
Kalehiwot <kalehiwotm@gmail.com>
2022-04-08 14:45:37 +0300

    Task 6 commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - Entery point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+char num;
+
+for (num = 48; num <= 57; num++)
+{
+putchar(num);
+}
+putchar('\n');
+
+return (0);
+}
