29343dd087ec78a3652afa8db8383734bdfb713b
Kalehiwot <kalehiwotm@gmail.com>
2022-04-08 14:50:52 +0300

    timmoc 7-ksaT

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - Entery point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+char alpha;
+
+for (alpha = 'z'; alpha >= 'a'; num--)
+{
+putchar(alpha);
+}
+putchar('\n');
+
+return (0);
+}
