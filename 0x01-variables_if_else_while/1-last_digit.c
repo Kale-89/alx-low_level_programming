56accc7c96542d5c5eba7aa2dda1276efe107834
Kalehiwot <kalehiwotm@gmail.com>
2022-04-08 13:56:30 +0300

    Task 1 commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ else if (n % 10 == 5)
 {
 printf("Last digit of %i is %i and is 0\n", n, n % 10);
 }
-else if (n % 10 != 0)
+else
 {
 printf("Last digit of %i is %i and is less than 6 and not 0\n", n, n % 10);
 }
