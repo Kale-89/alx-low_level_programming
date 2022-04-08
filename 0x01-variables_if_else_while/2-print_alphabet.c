763a4f5c54bc29b49facd56f709de3fa917d214a
Kalehiwot <kalehiwotm@gmail.com>
2022-04-08 14:18:03 +0300

    Task 2 commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entery point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char = alpha;
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+{
+putchar (alpha);
+}
+
+putchar(alpha);
+
+return (0);
+}
