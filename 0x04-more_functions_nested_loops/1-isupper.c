1ed231f0566989752c32b6ba6475fcff1f59e1a5
Kalehiwot <kalehiwotm@gmail.com>
2022-04-14 18:33:07 +0300

    Task 1 commit

---
+++ 0x04-more_functions_nested_loops/1-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @character: the character to be tracked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int _isupper(int character)
+{
+	return (character >= 'A' && character <= 'Z');
+}
