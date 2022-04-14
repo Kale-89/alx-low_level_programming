c36c904a0fa9d9685a27afe30a7c6b0f1fa73e19
Kalehiwot <kalehiwotm@gmail.com>
2022-04-14 18:39:29 +0300

    Task 0 commit

--- 0x04-more_functions_nested_loops/1-isupper.c
+++ 0x04-more_functions_nested_loops/1-isupper.c
@@ -5,9 +5,7 @@
  * @letter: the character to be tracked
  * Return: 1 if c is uppercase, 0 otherwise
  */
-
 int _isupper(int letter)
 {
-	if (letter >= 'A' && letter <= 'Z')
-	return (1);
+	return (letter >= 'A' && letter <= 'Z');
 }
