dabf0abb2c2427d7d0f24dde97c54ac31f448725
Kalehiwot <kalehiwotm@gmail.com>
2022-04-15 15:53:48 +0300

    Task 3 commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,20 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers, from 0 to 9
- */
+* print_numbers - prints the numbers, from 0 to 9
+*
+* Return: Always 0 (Success)
+*/
 
 void print_numbers(void)
 {
+int i;
 
-for (int i = 48; i < 58; i++)
+i = 0;
+
+for (int i = 0; i < 10; i++)
 {
-_putchar(i);
+_putchar(i + '0');
 }
-_putchar(10);
+_putchar('\n');
 }
