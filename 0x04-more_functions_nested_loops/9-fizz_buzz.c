182d87019922c0c5894fad8bc62305b4f595d62b
Kalehiwot <kalehiwotm@gmail.com>
2022-04-15 16:37:47 +0300

    Task 9 commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -22,7 +22,9 @@ printf(" Fizz");
 else if (i % 5 == 0 && i % 3 != 0)
 {
 printf(" Buzz");
-} else if (i % 3 == 0 && i % 5 == 0)
+}
+
+else if (i % 3 == 0 && i % 5 == 0)
 {
 printf(" FizzBuzz");
 } else if (i == 1)
