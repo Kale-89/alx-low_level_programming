706eb65068ce819bb532a6f577c09b218563d91a
Kalehiwot <kalehiwotm@gmail.com>
2022-04-26 12:27:41 +0300

    Task 1 commit

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - input
+ * @des: input pointer to string destination
+ * @sr: input pointer to source type string
+ * @n: number of bytes
+ * Return: pointer to destination string
+ */
+
+char *_memcpy(char *des, char *sr, unsigned int n)
+{
+unsigned int i = 0;
+
+while (i < n)
+{
+*(des + i) = *(sr + i);
+i++;
+}
+return (des);
+}
