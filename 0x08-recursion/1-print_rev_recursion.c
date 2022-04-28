0fbae5e1a71fd14e5d2ba8edf24967884b6fc6d7
Kalehiwot <kalehiwotm@gmail.com>
2022-04-28 18:38:27 +0300

    Task 1 commit

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,7 +5,7 @@
  * @s: string
  *
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * On error, -1 is returned, and error num is set appropriately.
  */
 void _print_rev_recursion(char *s)
 {
