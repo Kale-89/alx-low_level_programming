bd676967c75b886300c87dde2128652cbe5dbd6a
Kalehiwot <kalehiwotm@gmail.com>
2022-04-28 18:47:58 +0300

    Task 2 commit

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,7 +3,7 @@
 /**
  * _strlen_recursion - Returns Length of String
  * @s: string
- * @a: allocation
+ *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
@@ -12,8 +12,8 @@ int _strlen_recursion(char *s)
 {
 if (*s != '\0')
 {
-int a = 1 + _strlen_recursion(s + 1);
-return (a);
+
+return (1 + _strlen_recursion(s + 1));
 }
 return (0);
 }
